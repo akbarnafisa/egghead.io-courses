4377a65949be687a1ac766d4d92854b49a6499e6
Akbar Nafisa Ja'far <akbar.nafisa@dana.id>
2022-04-15 19:40:07 +0700

    chore: update lesson-4

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="app">
-    <p> {{ jobs[0].location }}</p>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue';
+import JobList from './components/JobList.vue';
 import { Job } from './types/Job';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {
+    JobList,
+  },
   setup () {
     // const state  = reactive({
     //   name: 'Link',
